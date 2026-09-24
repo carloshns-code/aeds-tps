3a0a49a0f18817e74cd55035755abb56d9f8aabe
carloshns-code <comp.materiais.chns@gmail.com>
2026-09-24 01:16:57 -0600

    alteraS 24-09

--- TP1/pokelista/pokelista.c
+++ TP1/pokelista/pokelista.c
@@ -1,37 +1,164 @@
 #include "pokelista.h"
-#include <stdio.h>
+#include <stdlib.h>
 
+void pl_init(Pokelista *pl)
+{
+    if (pl == NULL)
+    {
+        printf("Erro: ponteiro para Pokelista é NULL.\n");
+        return;
+    }
 
-/* CRIANDO A CABEÇA DA LISTA ENCADEADA */
+    pl->pPrimeiro = (Apontador)malloc(sizeof(TCelula));
 
-Pokelista* pl_init(TLista *pl) {     //esse TLista contem o typecast que define o tipo de ponteiro Apontador, que aponta para a estrutura Pokelista
+    if (pl->pPrimeiro == NULL)
+    {
+        pl->pUltimo = NULL;
+        return;
+    }
 
-    pl->primeiro = (Apontador) malloc(sizeof(Pokelista)); //aloca memoria para a cabeça da lista
-    pl->primeiro = pl->ultimo;  //Apontador = struct Pokelista*
-    pl->primeiro->prox = NULL; //inicializa o ponteiro para a proxima celula como NULL
+    pl->pUltimo = pl->pPrimeiro;
+    pl->pPrimeiro->pProx = NULL;
 }
 
-/* INSERINDO UM NOVO ELEMENTO NA LISTA */
+int pl_vazia(Pokelista *pl)
+{
+    if (pl == NULL)
+    {
+        return 1; // Considera a lista vazia se o ponteiro for NULL
+    }
+    else
+    {
+        pl->pPrimeiro = pl->pUltimo; // Se o ponteiro para a primeira célula for igual ao ponteiro para a última célula, a lista está vazia
+    }
+}
 
-Pokelista* pl_insere(Pokelista *pl, Pokemon p){
-    Pokelista *novo;
-    Pokelista *atual;
+int pl_insere(Pokelista *pl, Pokemon p)
+{
+    Apontador nova;
 
-    pl->ultimo->prox = (Apontador) malloc(sizeof(Pokelista)); //aloca memoria para a nova celula da lista
-    pl->ultimo = pl->ultimo->prox; //atualiza o ponteiro para a ultima celula da lista
-    pl->ultimo->pokemon = *p; //atribui o pokemon a nova celula da lista
-    pl->ultimo->prox = NULL; //inicializa o ponteiro para a proxima celula como NULL
-    
-    novo = (Pokelista*) malloc(sizeof(Pokelista)); //aloca memoria para a nova celula da lista
-    if (novo == NULL) {
-        printf("Erro ao alocar memoria para a nova celula da lista.\n");
-        return pl;  
+    if (pl == NULL || pl->pUltimo == NULL)
+    {
+        return 0; // Falha na inserção devido a ponteiro nulo
     }
 
-    novo->pokemon = p; //atribui o pokemon a nova celula da lista
-    novo->prox = NULL; //inicializa o ponteiro para a proxima celula como NULL
+    pl->pUltimo->prox = (Apontador)malloc(sizeof(TCelula));
 
-    if(pl==NULL){
-        return novo; //se a lista estiver vazia, retorna a nova celula como a primeira celula da lista
+    if (pl->pUltimo->pProx == NULL)
+    {
+        return 0; // Falha na alocação de memória
     }
+
+    pl->pUltimo = pl->pUltimo->pProx;
+
+    nova = pl->pUltimo;
+    nova->pokemon = p;
+    nova->pProx = NULL;
+
+    return 1;
+}
+
+Apontador pl_busca(Pokelista *pl, Pokemon p)
+{
+
+    Apontador atual;
+    int idBuscado;
+
+    if (pl == NULL || pl->pPrimeiro == NULL)
+    {
+        return NULL; // Lista vazia ou ponteiro nulo
+    }
+
+    idBuscado = p_get_id(&p);
+    atual = pl->pPrimeiro->pProx; // Começa a busca a partir da primeira célula válida
+
+    while (atual != NULL)
+    {
+        if (p_get_id(&(atual->pokemon)) == idBuscado)
+        {
+            return atual; // Retorna o ponteiro para a célula encontrada
+        }
+        atual = atual->pProx; // Avança para a próxima célula
+    }
+
+    return NULL; // Pokémon não encontrado
+}
+
+int pl_remove(Pokelista *pl, Pokemon p)
+{
+    Apontador anterior, atual;
+    int idBuscado;
+
+    if (pl == NULL || pl_vazia(pl))
+    {
+        return 0; // Lista vazia ou ponteiro nulo
+    }
+
+    idBuscado = p_get_id(&p);     // pega o id do pokemon que quero remover
+    atual = pl->pPrimeiro->pProx; // Começa a busca a partir da primeira célula válida
+    anterior = pl->pPrimeiro;     // celula cabeça
+
+    while (atual != NULL)
+    {
+
+        if (p_get_id(&(atual->pokemon)) == idBuscado)
+        {
+            anterior->pProx = atual->pProx; // Remove a célula. Encontrei o que eu quero, pego o anterior e aponto para onde ele aponta. Logo, a cadeia nao se quebra.
+
+            // Se eu removi o último, preciso atualizar pUltimo
+            if (atual == pl->pUltimo)
+            {
+                pl->pUltimo = anterior;
+            }
+
+            free(atual); // Libera a memória
+            return 1;    // Remoção bem-sucedida
+        }
+        anterior = atual;     // Avança o ponteiro anterior -> aqui tem uma escadinha!!!
+        atual = atual->pProx; // Avança o ponteiro atual
+    }
+
+    return 0; // Pokémon não encontrado
+}
+
+int pl_tamanho(Pokelista *pl)
+{
+    Apontador atual;
+    int tamanho = 0;
+
+    if (pl == NULL || pl->pPrimeiro == NULL)
+    {
+        return 0; // Lista vazia ou ponteiro nulo
+    }
+
+    atual = pl->pPrimeiro->pProx; // Começa a contagem a partir da primeira célula válida (apos a cabeça)
+    while (atual != NULL)
+    {
+        tamanho++;
+        atual = atual->pProx;
+    }
+
+    return tamanho;
+}
+
+void pl_libera(Pokelista *pl)
+{
+    Apontador atual, proximo;
+
+    if (pl == NULL)
+    {
+        return; // Ponteiro nulo
+    }
+
+    atual = pl->pPrimeiro->pProx; // Começa a partir da cabeça primeira celula apos a cabeça
+
+    while (atual != NULL)
+    {
+        proximo = atual->pProx; // Armazena o próximo ponteiro (primeira celula apos a cabeça)
+        free(atual);            // Libera a célula atual
+        atual = proximo;        // Avança para a próxima célula
+    }
+
+    pl->pPrimeiro = NULL; // Define os ponteiros como NULL após liberar a lista
+    pl->pUltimo = NULL;
 }
--- TP1/pokelista/pokelista.h
+++ TP1/pokelista/pokelista.h
@@ -1,33 +1,32 @@
 #ifndef POKELISTA_H
 #define POKELISTA_H
 
-#include<stdlib.h>
 #include "../pokemon/pokemon.h"
 
-typedef struct pokelista* Apontador;      //Apontador = struct Pokelista, ou seja, é um ponteiro para a estrutura pokelista, que representa uma celula da lista  
-typedef struct pokelista {      //estrutura que representa uma celula da lista
-    Pokemon pokemon;               //objeto do tipo Pokemon que sera armazenado na celula da lista
-    struct pokelista *prox;      //esse ponteiro para a proxima celula quarda o endereco da proxima celula da lista
+typedef struct Celula *Apontador;
+typedef struct Celula
+{
+    Pokemon pokemon;
+    struct Celula *prox;
+} TCelula;
+
+typedef struct
+{
+    Apontador pPrimeiro;
+    Apontador pUltimo;
 } Pokelista;
 
-
-typedef struct {
-    Apontador primeiro;           //ponteiro para a primeira celula da lista
-    Apontador ultimo;              //ponteiro para a ultima celula da lista
-    int tamanho;                 //quantidade de elementos na lista
-} TLista;
-
-Pokelista* pl_init();
-
-Pokelista* pl_insere(Pokelista *pl, Pokemon p);
-
-Pokelista* pl_remove(Pokelista *pl, Pokemon p);
-
-Pokelista* pl_busca(Pokelista *pl, Pokemon p);
-
-void pl_imprime(Pokelista *pl);
+void pl_init(Pokelista *pl);
 
 int pl_vazia(Pokelista *pl);
+
+int pl_insere(Pokelista *pl, Pokemon p);
+
+int pl_remove(Pokelista *pl, Pokemon p);
+
+Apontador pl_busca(Pokelista *pl, Pokemon p);
+
+void pl_imprime(Pokelista *pl);
 
 void pl_libera(Pokelista *pl);
 
